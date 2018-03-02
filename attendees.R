99e5bba92ee1436da4269a8dc8ac5e4c5e29df5d
Phillip Doehle <doehle@okstate.edu>
2018-03-02 16:42:05 -0600

    Add attendees.R to repo

---
+++ attendees.R
@@ -0,0 +1,24 @@
+#!/usr/bin/Rscript
+library("tidyverse")
+library("shiny")
+
+args = commandArgs(trailingOnly = T)
+if (length(args)==0){
+  stop("Need an input .csv file!\n", call. = F)
+} else if (length(args)==1){
+  args[2] = "out.csv"
+}
+
+attendees <- read_csv(args[1])
+
+attendees <- attendees %>% 
+  select(order_date = 'Order Date', first = 'First Name', 
+         last = 'Last Name', email = 'Email', affiliation = 'Please Specify', 
+         status = 'Status', department = 'Department (i.e. Chemistry)') %>% 
+  mutate(affiliation = ifelse(is.na(affiliation), 
+                              "Oklahoma State University", affiliation))
+
+#prompt01 <- "Enter workshop dates as space separated list in YYYY-MM-DD format.\n"
+#ws_dates <- as.Date(strsplit(readline(prompt01), " ")[[1]])
+
+attendees %>% write_csv(path = args[2])
